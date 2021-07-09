109aa4675309304601aeabfedd8fe78adf8ab5c4
Daniel De Pinto <daniel.depinto@gmail.com>
2021-07-09 09:53:46 -0700

    day06

--- aoc/2020/pkg/utils/loadInputFile.go
+++ aoc/2020/pkg/utils/loadInputFile.go
@@ -7,7 +7,7 @@ import (
 )
 
 func LoadInputFile(name string) ([]string, error) {
-	absName, err := filepath.Abs("input/" + name)
+	absName, err := filepath.Abs("../../input/" + name)
 	if err != nil {
 		return nil, err
 	}
@@ -22,6 +22,38 @@ func LoadInputFile(name string) ([]string, error) {
 	scanner := bufio.NewScanner(ifh)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func LoadInputFileV2(name string) ([]string, error) {
+	absName, err := filepath.Abs("../../input/" + name)
+	if err != nil {
+		return nil, err
+	}
+
+	ifh, err := os.Open(absName)
+	if err != nil {
+		return nil, err
+	}
+	defer ifh.Close()
+
+	lines := []string{}
+	line := ""
+	scanner := bufio.NewScanner(ifh)
+	for scanner.Scan() {
+		newLine := scanner.Text()
+		if len(newLine) != 0 {
+			line = line + newLine
+		} else {
+			lines = append(lines, line)
+			line = ""
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
